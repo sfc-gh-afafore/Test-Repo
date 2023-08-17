1fa0afedf3ae68916acfa2556993bf85cd1f5ebf
Ayomitunde Fafore <ayomitunde.fafore@snowflake.com>
2023-08-17 10:13:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.write('How is the day going.')
-st.write('How is the day going today. I hope you are doing well.')
+st.write('How is the day going today. I hope you are doing well today.')
