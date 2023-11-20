b372d884a912ebaeb59a7976617b891f7db6f34c
Ayomitunde Fafore <ayomitunde.fafore@snowflake.com>
2023-11-20 16:19:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
-st.write('How is the day going.')
-st.write('How is the day going today. I hope you are doing well today.')
+with open("../../../home/appuser/.streamlit/dataset-credentials.p8", "rb") as key:
+      p_key= serialization.load_pem_private_key(
+          key.read(),
+          password=None,
+          backend=default_backend()
+      )
+
+    pkb = p_key.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption())
+
+    conn = st.experimental_connection("snowpark", private_key=pkb, role="readonly_role")
+    query = conn.query('select * from free_dataset_GZ1M6Z2R41Y.public.t_rbaseit limit 10;');
+    st.dataframe(query)
+
